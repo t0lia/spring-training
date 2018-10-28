aae0ac6d479e1b5a624e603591ee4c87790e53b4
t0lia <t0lia@yandex.ru>
2018-10-28 09:14:27 +0300

    lab3. create module

--- lab3/src/main/java/spring_training/SpringTempApplication.java
+++ lab3/src/main/java/spring_training/SpringTempApplication.java
@@ -1,4 +1,4 @@
-package com.company.spring_training;
+package spring_training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
