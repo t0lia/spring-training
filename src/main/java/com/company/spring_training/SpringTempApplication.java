94e766e76e925422ca3c5b7e63c8b28652f1daa7
t0lia <t0lia@yandex.ru>
2018-10-28 08:44:21 +0300

    Spring boot initialize

---
+++ src/main/java/com/company/spring_training/SpringTempApplication.java
@@ -0,0 +1,12 @@
+package com.company.spring_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTempApplication.class, args);
+    }
+}
