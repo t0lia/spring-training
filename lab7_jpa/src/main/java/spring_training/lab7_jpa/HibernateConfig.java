9c401e3261ac31f72f181e133049908cc0d34cf6
t0lia <t0lia@yandex.ru>
2018-11-05 14:34:49 +0300

    lab7_jpa

---
+++ lab7_jpa/src/main/java/spring_training/lab7_jpa/HibernateConfig.java
@@ -0,0 +1,64 @@
+package spring_training.lab7_jpa;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan
+@EnableTransactionManagement
+public class HibernateConfig {
+
+    @Bean
+    public DataSource h2DataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("sa");
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(h2DataSource());
+        em.setPackagesToScan("spring_training.lab7_jpa.model");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+
+        return properties;
+    }
+}
---
+++ lab7_jpa/src/test/java/spring_training/lab7_jpa/ApplicationTest.java
@@ -0,0 +1,85 @@
+package spring_training.lab7_jpa;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import spring_training.lab7_jpa.model.Country;
+import spring_training.lab7_jpa.model.CountryDao;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = HibernateConfig.class)
+class ApplicationTest {
+
+    private static Logger log = LogManager.getLogger(ApplicationTest.class);
+
+
+    private List<Country> persistedCountries;
+
+    private Country rus = new Country("Russia", "RUS");
+    private Country usa = new Country("United States of America", "USA");
+    private Country gb = new Country("Great Britain", "GB");
+    private Country fra = new Country("France", "FRA");
+
+    @Autowired
+    private CountryDao countryDao;
+
+    @BeforeEach
+    void setUp() {
+        persistedCountries = Stream.of(rus, usa, gb, fra)
+                .peek(countryDao::save)
+                .collect(Collectors.toList());
+    }
+
+    @Test
+    void testGetAll() {
+        List<Country> allCountries = countryDao.getAllCountries();
+        assertEquals(allCountries, persistedCountries);
+    }
+
+    @Test
+    void testStartWith() {
+        List<Country> startWithR = countryDao.getStartWith("R");
+        assertEquals(Collections.singletonList(rus), startWithR);
+        List<Country> startWithUnited = countryDao.getStartWith("United");
+        assertEquals(startWithUnited, Collections.singletonList(usa));
+    }
+
+    @Test
+    void testFindByName() {
+        countryDao.getCountryByName("France")
+                .map(x -> {
+                    assertEquals(fra, x);
+                    return x;
+                })
+                .orElseThrow(() -> new AssertionError("Can't find"));
+    }
+
+    @Test
+    void testUpdate() {
+        countryDao.getCountryByName("Russia")
+                .map(country -> country.setName("USSR"))
+                .ifPresent(countryDao::update);
+
+        countryDao.getCountryByName("USSR").orElseThrow(() -> new AssertionError("can't find updated value"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        countryDao.clear();
+    }
+}
